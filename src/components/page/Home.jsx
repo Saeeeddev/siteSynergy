5f7aac0c0aeeed5df698e914d32ea02141f20a2b
saeed <dolatisaeed6@gmail.com>
2025-09-06 02:44:51 +0330

    feat: Add EmptySection component and update CompanyLogos styles

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../website/Hero';
 import HeroVideoDemo from '../website/HeroVideoDemo';
 import CompanyLogos from '../website/CompanyLogos';
+import EmptySection from '../website/EmptySection';
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Hero />
       <HeroVideoDemo />
       <CompanyLogos />
+      <EmptySection />
     </main>
   );
 }
---
+++ src/components/website/EmptySection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../style/EmptySection.css';
+
+function EmptySection() {
+  return (
+    <div className="empty-section__container">
+      <div className="empty-section__content">
+        {/* Content will go here */}
+      </div>
+    </div>
+  );
+}
+
+export default EmptySection;
